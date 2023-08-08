51b7455aa0a80d6f0153b2cef4c10b050631f5c5
Cristian Dávila <krif07@gmail.com>
2023-08-07 20:38:34 -0500

    Chaged project for cucumber and junit4

--- src/test/java/starter/navigation/NavigateTo.java
+++ src/test/java/starter/navigation/NavigateTo.java
@@ -1,11 +1,11 @@
-package starter.actions.navigation;
+package starter.navigation;
 
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
 
 public class NavigateTo {
-    public static Performable theSearchHomePage() {
+    public static Performable theDuckDuckGoHomePage() {
         return Task.where("{0} opens the DuckDuckGo home page",
                 Open.browserOn().the(DuckDuckGoHomePage.class));
     }
